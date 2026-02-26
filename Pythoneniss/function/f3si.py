1fe12793aa2ea54fc06b41bb735f245aa606e6a1
unknown <mohapatrajagadish89@gmail.com>
2026-02-27 00:21:02 +0530

    function

---
+++ Pythoneniss/function/f3si.py
@@ -0,0 +1,11 @@
+def sical(p,t,r):
+	si=p*t*r/100
+	print("simple=",si)
+	return
+print("enter principal")
+p=float(input())
+print("enter time")
+t=float(input())
+print("enter rate")
+r=float(input())
+sical(p,t,r)	
---
+++ Pythoneniss/function/f4.py
@@ -0,0 +1,9 @@
+def check():
+	if n%2==0:
+		print("even number")
+	else:
+	   print("odd number")
+print("enter a number")
+n=int(input())
+check()
+
