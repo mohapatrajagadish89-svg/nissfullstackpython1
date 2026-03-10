17a0327a941e03611f4a8f104433b135252dee14
unknown <mohapatrajagadish89@gmail.com>
2026-03-10 22:29:00 +0530

    oop.py

---
+++ Pythoneniss/oop.py/con1.py
@@ -0,0 +1,12 @@
+class Demo:
+	def __init__(self):
+		self.x=int(input())
+		self.y=int(input()) #instance variable
+print("enter objects 2 values")
+ob=Demo()	
+print("enter objects 2 values")
+ob1=Demo()	
+print("display first objects values")
+print(ob.x,ob.y)
+print("display second objects values")
+print(ob1.x,ob1.y)
